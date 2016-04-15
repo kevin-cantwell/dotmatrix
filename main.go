896b40a996c2866fdaab96f38daa0e676c4027b3
Kevin Cantwell <kevin.cantwell@gmail.com>
2016-04-15 18:18:22 -0400

    resize inputs to terminal widths and invert monochrome

--- main.go
+++ main.go
@@ -3,58 +3,63 @@ package main
 import (
 	"fmt"
 	"image"
+	_ "image/gif"
 	_ "image/jpeg"
+	_ "image/png"
 	"os"
+	"syscall"
+	"unsafe"
+
+	"github.com/nfnt/resize"
+)
+
+type dot int
+
+const (
+	black dot = 1
+	white dot = 0
 )
 
 func main2() {
 	var dots pattern
-	dots[0] = [4]int{
-		0,
-		1,
-		0,
-		1,
-	}
-	dots[1] = [4]int{
-		1,
-		0,
-		1,
-		0,
+	dots[0] = [4]dot{
+		white,
+		black,
+		white,
+		black,
 	}
 	fmt.Println(dots.String())
 }
 
 func main() {
-	// for i := int32(0); i < 16*16; i++ {
-	// 	c := '\u2800' + i
-	// 	fmt.Printf("%d, %c⣿\n", c, c)
-	// }
-	// spinner := []rune{'⣾', '⣷', '⣯', '⣟', '⡿', '⢿', '⣻', '⣽'}
+	img, _, err := image.Decode(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
 
-	f, err := os.Open("mono.jpg")
+	w, _, err := GetTerminalSize()
 	if err != nil {
 		panic(err)
 	}
-	jpg, t, err := image.Decode(f)
-	if err != nil {
-		panic(err)
-	}
-	f.Close()
-	fmt.Printf("%T %s\n", jpg, t)
+
+	w = w * 2 // Since each symbol is two dots wide
+
+	img = resize.Thumbnail(uint(w), uint(img.Bounds().Dy()), img, resize.NearestNeighbor)
 
 	var picture string
 
-	dx := jpg.Bounds().Dx()
-	dy := jpg.Bounds().Dy()
+	bounds := img.Bounds()
 
-	// Create symbols, left-right & top-bottom.
-	for py := 0; py < dy; py += 4 {
-		for px := 0; px < dx; px += 2 {
+	// An image's bounds do not necessarily start at (0, 0), so the two loops start
+	// at bounds.Min.Y and bounds.Min.X. Looping over Y first and X second is more
+	// likely to result in better memory access patterns than X first and Y second.
+	for py := bounds.Min.Y; py < bounds.Max.Y; py += 4 {
+		for px := bounds.Min.X; px < bounds.Max.X; px += 2 {
 			var dots pattern
 			// Draw left-right, top-bottom.
 			for y := 0; y < 4; y++ {
 				for x := 0; x < 2; x++ {
-					dots[x][y] = dotAt(jpg, px+x, py+y)
+					dots[x][y] = dotAt(img, px+x, py+y)
 				}
 			}
 			picture += dots.String()
@@ -65,12 +70,33 @@ func main() {
 	fmt.Println(picture)
 }
 
-func dotAt(img image.Image, x, y int) int {
-	_, _, _, r := img.At(x, y).RGBA()
-	if r > 0 {
-		return 1
+// func dotAt(img image.Image, x, y int) dot {
+// 	r, g, b, a := img.At(x, y).RGBA()
+// 	fmt.Printf("(%d,%d) %d, %d, %d, %d\n", x, y, r, g, b, a)
+// 	if a < 65535 {
+// 		return black
+// 	}
+// 	if r < 65535 {
+// 		return white
+// 	}
+// 	return black
+// }
+
+// 32,767 is half bright
+func dotAt(img image.Image, x, y int) dot {
+	v := grayByLuminosity(img.At(x, y).RGBA())
+	// fmt.Printf("(%d,%d) %d, %d, %d, %d\n", x, y, r, g, b, a)
+	// fmt.Printf("(%d,%d) %d\n", x, y, v)
+	if v <= 30767 {
+		return white
 	}
-	return 0
+	return black
+}
+
+// 0.21 R + 0.72 G + 0.07 B
+func grayByLuminosity(r, g, b, a uint32) uint32 {
+	weighted := 0.21*float32(r) + 0.72*float32(g) + 0.07*float32(b)
+	return uint32(weighted)
 }
 
 // Represents an 8 dot braille pattern using x,y coordinates. Eg:
@@ -80,7 +106,7 @@ func dotAt(img image.Image, x, y int) int {
 // |(0,2)(1,2)|
 // |(0,3)(1,3)|
 // +----------+
-type pattern [2][4]int
+type pattern [2][4]dot
 
 // CodePoint maps each point in pattern to a braille number and
 // calculates the corresponding unicode symbol.
@@ -92,14 +118,30 @@ type pattern [2][4]int
 // +------+
 // See https://en.wikipedia.org/wiki/Braille_Patterns#Identifying.2C_naming_and_ordering)
 func (dots pattern) CodePoint() rune {
-	lowEndian := [8]int{dots[0][0], dots[0][1], dots[0][2], dots[1][0], dots[1][1], dots[1][2], dots[0][3], dots[1][3]}
+	lowEndian := [8]dot{dots[0][0], dots[0][1], dots[0][2], dots[1][0], dots[1][1], dots[1][2], dots[0][3], dots[1][3]}
 	var v int
 	for i, x := range lowEndian {
-		v += x << uint(i)
+		v += int(x) << uint(i)
 	}
 	return rune(v) + '\u2800'
 }
 
 func (dots pattern) String() string {
 	return string(dots.CodePoint())
+}
+
+func GetTerminalSize() (width, height int, err error) {
+	var dimensions [4]uint16
+
+	_, _, e := syscall.Syscall6(
+		syscall.SYS_IOCTL,
+		uintptr(syscall.Stdout),
+		uintptr(syscall.TIOCGWINSZ),
+		uintptr(unsafe.Pointer(&dimensions)),
+		0, 0, 0,
+	)
+	if e != 0 {
+		return -1, -1, e
+	}
+	return int(dimensions[1]), int(dimensions[0]), nil
 }
