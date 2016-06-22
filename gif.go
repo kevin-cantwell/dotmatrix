5026aea7dbaeadff794f7308fcb0a366fad9d920
Kevin Cantwell <kevin.cantwell@gmail.com>
2016-06-21 21:52:11 -0400

    fixes background gif disponsal

--- gif.go
+++ gif.go
@@ -17,7 +17,17 @@ the cursor at the beginning of each frame. Delays and disposal methods are
 respected.
 */
 func PlayGIF(w io.Writer, giff *gif.GIF) error {
+	if len(giff.Image) < 1 {
+		return nil
+	}
+
+	// The screen is what we flush to the writer on each iteration
 	var screen *image.Paletted
+	// Only used if we see background disposal methods
+	bgPallette := color.Palette{color.Transparent}
+	if p, ok := giff.Config.ColorModel.(color.Palette); ok {
+		bgPallette = p
+	}
 
 	for c := 0; giff.LoopCount == 0 || c < giff.LoopCount; c++ {
 		for i := 0; i < len(giff.Image); i++ {
@@ -26,38 +36,42 @@ func PlayGIF(w io.Writer, giff *gif.GIF) error {
 
 			// Always draw the first frame from scratch
 			if i == 0 {
-				screen = frame
+				screen = convert(image.NewPaletted(frame.Bounds(), bgPallette))
 			}
 
 			switch giff.Disposal[i] {
+
 			// Dispose previous essentially means draw then undo
 			case gif.DisposalPrevious:
-				previous := image.NewPaletted(screen.Bounds(), screen.Palette)
-				copy(previous.Pix, screen.Pix)
-				drawFrame(screen, frame)
+				temp := image.NewPaletted(screen.Bounds(), screen.Palette)
+				copy(temp.Pix, screen.Pix)
+
+				drawOver(screen, frame)
 				if err := flush(w, screen); err != nil {
 					return err
 				}
-				screen = previous
 				<-delay
+
+				screen = temp
+
 			// Dispose background replaces everything just drawn with the background canvas
 			case gif.DisposalBackground:
-				background := image.NewPaletted(frame.Bounds(), frame.Palette)
-				for i := 0; i < len(background.Pix); i++ {
-					background.Pix[i] = 2
-				}
-				drawFrame(screen, frame)
+				background := convert(image.NewPaletted(frame.Bounds(), bgPallette))
+				drawExact(screen, background)
+				temp := image.NewPaletted(screen.Bounds(), screen.Palette)
+				copy(temp.Pix, screen.Pix)
+
+				drawOver(screen, frame)
 				if err := flush(w, screen); err != nil {
 					return err
 				}
 				<-delay
-				drawFrame(screen, background)
-				if err := flush(w, screen); err != nil {
-					return err
-				}
+
+				screen = temp
+
 			// Dispose none or undefined means we just draw what we got over top
 			default:
-				drawFrame(screen, frame)
+				drawOver(screen, frame)
 				if err := flush(w, screen); err != nil {
 					return err
 				}
@@ -68,7 +82,8 @@ func PlayGIF(w io.Writer, giff *gif.GIF) error {
 	return nil
 }
 
-func drawFrame(target *image.Paletted, source image.Image) {
+// Draws any non-transparent pixels into target
+func drawOver(target *image.Paletted, source image.Image) {
 	bounds := source.Bounds()
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
@@ -77,6 +92,16 @@ func drawFrame(target *image.Paletted, source image.Image) {
 				continue
 			}
 			target.Set(x, y, c)
+		}
+	}
+}
+
+// Draws pixels into target, including transparent ones.
+func drawExact(target *image.Paletted, source image.Image) {
+	bounds := source.Bounds()
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			target.Set(x, y, source.At(x, y))
 		}
 	}
 }
